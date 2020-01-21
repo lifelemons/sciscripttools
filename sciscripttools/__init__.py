5b30268ae171c6e0a35929221bfad7f43aa4a936
Punit <punitpatel@protonmail.com>
2020-01-21 17:39:34 +0000

    Edit: change scope of functions in plot.py
    only bring up a few class and function into the scope of the package

--- sciscripttools/__init__.py
+++ sciscripttools/__init__.py
@@ -2,4 +2,4 @@
 from .generic import create_dictionary
 from .conversion import dictionary_to_arrays, dictionary_items_to_numpy_arrays
 from .io import load_data, load_item, save_data
-from .plot import *
+from .plot import figure_parameters, standard_font, standard_figure, move_view
